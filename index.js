bfb2876e2676073084f4f7c868aad3ecef707612
Abdullah Bin Rashid <71758955+abdullah19191@users.noreply.github.com>
2023-02-22 23:06:21 +0500

    Chat Server Socket.io

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+var http = require("http");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+const socket = require("socket.io");
+//middleware
+app.use(express.json());
+app.use(express.static("public"));
+
+server.listen(port,"0.0.0.0", function (){
+    console.log("server started");
+    console.log(`Listening on port ${port}`);
+    console.log(`http://localhost:${port}`);
+});
+
+//Checking For Connection
+app.route("/check").get((res,req)=>{
+  return res.json("Your App Is Running Successfully!!");
+})
+// Socket setup
+const io = socket(server);
+
+var clients = {}
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+  console.log(socket.id ,"socket id has joined");
+  //Listening to FrontEnd 
+// signing in user
+  socket.on("signin",(id)=>{
+         console.log(id);
+         clients[id] = socket;
+  });
+
+  //sending message
+  socket.on("message",(msg)=>{
+    let targetId = msg.targetId;
+    if(clients[targetId]) clients[targetId].emit("message",msg);
+    console.log(msg);
+});
+});
